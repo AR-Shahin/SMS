88184f84e039d1f1154d70e704be412cb4128789
AR Shahin <mdshahinmije96@gmail.com>
2021-08-12 11:54:10 +0600

    dept admin seeder change

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -26,7 +26,7 @@ class DatabaseSeeder extends Seeder
         DepartmentAdmin::create([
             'name' => 'Department Admin',
             'email' => 'dept-admin@mail.com',
-            'password' => bcrypt('password'),
+            'password' => 'password',
             'department_id' => 1
         ]);
 
